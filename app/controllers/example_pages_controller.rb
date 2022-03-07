a2b39b44ddeeb5c252e3e6b0a9407a63a009065e
erwin <winzon353535@gmail.com>
2022-03-07 10:11:54 -0800

    updated sample app controller

--- app/controllers/example_pages_controller.rb
+++ app/controllers/example_pages_controller.rb
@@ -1,2 +1,13 @@
 class ExamplePagesController < ApplicationController
+  def goodbye_method
+    render json: [{message: "uhhh goodbye", color: "blue", size: ["banana", "two bananas"]}]
+  end
+
+  def stuff_method
+    render html: {message: "there is some stuff in here", color: ["red", "blue", "orange"]}
+  end
+
+  def new_method
+    render xml: {message: "input new new new new new"}
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get "goodbye_path", controller: "example_pages", action: "goodbye_method"
+
+  get "stuff_path", controller: "example_pages", action: "stuff_method"
+
+  get "new_path", controller: "example_pages", action: "new_method"
 end
